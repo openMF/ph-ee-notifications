ba697c1e944d33130e9d1bcbeb9ce570d2341661
Ankita Ranjan <ankita@fynarfin.io>
2021-12-30 20:06:15 +0530

    Adding success worker and appropriate messages using apache velocity

---
+++ src/main/java/org/mifos/connector/notification/sms/dto/MessageCreationDto.java
@@ -0,0 +1,39 @@
+package org.mifos.connector.notification.sms.dto;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.support.DefaultExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+import static org.mifos.connector.notification.camel.config.CamelProperties.*;
+
+import static org.mifos.connector.notification.zeebe.ZeebeVariables.*;
+import static org.mifos.connector.notification.zeebe.ZeebeVariables.TRANSACTION_ID;
+
+@Component
+public class MessageCreationDto {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    DateFormat df = new SimpleDateFormat("dd:MM:yy:HH:mm:ss");
+
+    public Exchange setPropertiesForMessage(Map<String, Object> variables){
+        String transactionId = (String) variables.get(TRANSACTION_ID);
+        String account = (String) variables.get(ACCOUNT);
+        String originDate = (String) variables.get(ORIGIN_DATE);
+        originDate = df.format(originDate);
+        String amount = (String) variables.get(AMOUNT);
+        Exchange exchange = new DefaultExchange(camelContext);
+        exchange.setProperty(CORRELATION_ID, transactionId);
+        exchange.setProperty(DATE, originDate);
+        exchange.setProperty(ACCOUNT_ID,account);
+        exchange.setProperty(TRANSACTION_AMOUNT,amount);
+        return exchange;
+    }
+}
--- src/main/java/org/mifos/connector/notification/zeebe/ZeebeVariables.java
+++ src/main/java/org/mifos/connector/notification/zeebe/ZeebeVariables.java
@@ -14,5 +14,8 @@ public class ZeebeVariables {
     public static final String MESSAGE_TO_SEND = "deliveryMessage";
     public static final String MESSAGE_INTERNAL_ID = "internalId";
     public static final String PHONE_NUMBER = "phoneNumber";
-    public static final int CALLBACK_RETRY_COUNT = 0;
+    public static final String CALLBACK_RETRY_COUNT = "callbackRetry";
+    public static final String ORIGIN_DATE = "originDate";
+    public static final String ACCOUNT = "accountId";
+    public static final String AMOUNT = "amount";
 }
