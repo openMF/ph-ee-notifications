4dd058445bb79852828900fc5897f2f5fcfcb3cf
Vic Romero <victor.romero@fintecheando.mx>
2022-04-25 00:02:59 -0500

    Non used variables and imports were removed

--- src/main/java/org/mifos/connector/notification/sms/utility/NotificationUtility.java
+++ src/main/java/org/mifos/connector/notification/sms/utility/NotificationUtility.java
@@ -1,11 +1,7 @@
 package org.mifos.connector.notification.sms.utility;
 
-import org.mifos.connector.common.channel.dto.TransactionChannelCollectionRequestDTO;
-import org.mifos.connector.notification.sms.dto.MessageResponseDto;
-
 public class NotificationUtility {
-
-
-    }
+    
+}
 
 
